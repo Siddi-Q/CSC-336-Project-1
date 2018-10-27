fed972a430ee05a96f42e5cb474df685d881b8ac
Saddique Shafi <saddiqueashafi@gmail.com>
2018-10-26 21:12:54 -0400

    Added templates and app.py files

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'saddique'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'mydb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route("/test")
+def testing():
+    cursor = mysql.connect().cursor()
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    return render_template('test.html', tables = tables)
+
+@app.route("/")
+def hello():
+    return "<h1>Home Page<h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
